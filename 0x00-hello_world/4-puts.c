f4e3e823819f40848aea44fcc51a824c2f93d0fb
Michelle Thuo <92251779+M1CH133@users.noreply.github.com>
2022-09-08 13:01:03 +0300

    Task 4: Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main: Starting point of the program
+ * Returns 0 if the program is successfully compiled
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
