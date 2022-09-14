9481af4ef2718c578c90f3b6d7972d34940f42cb
Michelle Thuo <92251779+M1CH133@users.noreply.github.com>
2022-09-15 00:38:14 +0300

    Task 0: Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * main - Test whether the function positive_or_negative()
+  * gives the correct output when given a case of 0
+  * Return: 0 if successfully compiled, non-zero otherwise
+  */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+return (0);
+}
