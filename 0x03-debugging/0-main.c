a0e86e1324d9268280636d35ef65e5b0e23d2dcf
Michelle Thuo <92251779+M1CH133@users.noreply.github.com>
2022-09-15 00:58:02 +0300

    Task 0: Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+  * main - Test whether the function postitive_or_negative()
+  * gives the correct output when given a case of 0
+  * Return: 0 if successfuelly compiled, non-zero otherwise
+  */
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 98;
-        positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
+return (0);
 }
