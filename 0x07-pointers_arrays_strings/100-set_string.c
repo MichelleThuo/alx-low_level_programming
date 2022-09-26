65ec70c9d19a528201a4640197bdc13e3638c99f
Michelle Thuo <92251779+M1CH133@users.noreply.github.com>
2022-09-27 01:56:03 +0300

    Task 8: Double pointer, double fun

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,17 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * set_string - Sets the value of a pointer to char
+ * @s: the value to modify
+ * @to: the value to assign
  *
- * Return: Always 0.
+ * Return: Nothing.
  */
-int main(void)
+void set_string(char **s, char *to)
 {
-    char *s0 = "Bob Dylan";
-    char *s1 = "Robert Allen";
-
-    printf("%s, %s\n", s0, s1);
-    set_string(&s1, s0);
-    printf("%s, %s\n", s0, s1);
-    return (0);
+    *s = to;
 }
